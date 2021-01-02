32b917c201a8084fe52c5891302b0b2326196358
Clara Chick <clara.chick@mail.utoronto.ca>
2021-01-02 16:09:04 -0500

    :x:

--- src/components/constants/Images.js
+++
@@ -1,9 +0,0 @@
-import image1 from "../../imgs/header/star.png";
-import image2 from "../../imgs/header/flower.png";
-import image3 from "../../imgs/header/heart.png";
-
-export const images = {
-  image1,
-  image2,
-  image3,
-};
